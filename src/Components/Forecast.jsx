1cb1d608ada319969dd3cc4610fd1270f75b5ac7
Denis Brester <49287397+DenKick@users.noreply.github.com>
2020-09-21 00:56:20 +0300

    Working forecast and weather code file

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -3,16 +3,60 @@ import ForecastCard from './ForecastCard';
 import styles from './Forecast.module.css';
 
 export default class Forecast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    let that = this;
+
+    navigator.geolocation.watchPosition(function(position) {
+
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+
+      let apiKey = `3f323ac45ffd4f0d83323fa593986f16`;
+      let apiCall = `https://api.weatherbit.io/v2.0/forecast/daily?&days=6&lat=${lat}&lon=${lon}&key=${apiKey}`;
+
+      fetch(apiCall)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result.data);
+        that.setState({
+          isLoaded: true,
+          items: result.data
+        });
+      },
+        error => {
+          that.setState({
+            isLoaded: true,
+            error: error
+          })
+        }
+      )
+    })
+  }
+
   render() {
-    return (
-      <div className = {styles.forecastContainer}>
-        <ForecastCard />
-        <ForecastCard />
-        <ForecastCard />
-        <ForecastCard />
-        <ForecastCard />
-        <ForecastCard />
-      </div>
-    )
+    const { isLoaded, error, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading</div>
+    } else if (error) {
+      return <div>Error</div>
+    } else {
+      return (
+        <div className = {styles.forecastContainer}>
+          {items.map(item => (
+            <ForecastCard key = {item.datetime} day = {item.datetime} temp = {item.high_temp} icon = {item.weather.icon} />
+          ))}
+        </div>
+      )
+    }
   }
 }
--- src/Components/ForecastCard.jsx
+++ src/Components/ForecastCard.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import styles from './ForecastCard.module.css';
 
+import weatherCode from './weather';
+
 export default class ForecastCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      day: 'Mon',
-      icon: 'wi wi-day-sunny',
-      temp: '25'
+      day: new Date(Date.parse(this.props.day)),
+      icon: this.props.icon,
+      temp: this.props.temp
     }
   }
   render() {
     const {day, icon, temp} = this.state;
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
     return (
       <div className = {styles.cardContainer} >
-        <span className = {styles.day}>{day}</span>
-        <i className = {icon}></i>
-        <span className = {styles.temp}>{temp}°</span>
+        <span className = {styles.day}>{days[day.getDay()]}</span>
+        <i className = {weatherCode[icon]}></i>
+        <span className = {styles.temp}>{Number(temp).toFixed(0)}°</span>
       </div>
     )
   }
