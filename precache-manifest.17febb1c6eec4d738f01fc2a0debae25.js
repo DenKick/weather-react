117988e676743a537ff3dd76ca6f357e8803484b
Denis Brester <49287397+DenKick@users.noreply.github.com>
2020-09-20 17:06:38 +0300

    Updates

---
+++ precache-manifest.17febb1c6eec4d738f01fc2a0debae25.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6e9311ef2802c9c7215772e2b27875",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "de3a1935bdc0e778ab7c",
+    "url": "/weather-react/static/css/main.7735bec8.chunk.css"
+  },
+  {
+    "revision": "010d14df675393cbbe44",
+    "url": "/weather-react/static/js/2.b7869e69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.b7869e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3a1935bdc0e778ab7c",
+    "url": "/weather-react/static/js/main.5bdb225c.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
